110e74b909882ae0b2b6cf5f910c05546c97ff27
Kayke Barbosa <kaykeloiola@gmail.com>
2023-09-17 15:12:46 -0300

    add new commit: routes

---
+++ upload-ai-api/src/lib/openai.ts
@@ -0,0 +1,7 @@
+import "dotenv/config"
+import { OpenAI } from "openai"
+
+
+export const openai = new OpenAI({
+  apiKey: process.env.OPENAI_KEY,
+})
--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,10 +1,14 @@
 import { fastify } from "fastify";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
 
 const app = fastify()
 
-app.get('/', () =>{
-  return 'Hello World'
-})
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+
 
 app.listen({
   port: 3333,
